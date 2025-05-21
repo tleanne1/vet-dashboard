664e29dee6cb440411a5004cbb893dcbdb372cb1
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2025-05-21 11:20:34 -0500

    Updated responsiveness

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-const Layout = ({ children }) => {
-  return (
-    <div className="min-h-screen bg-gray-50 text-gray-800">
-      <Navbar />
-      <main>{children}</main>
-    </div>
-  )
-}
+const Layout = ({ children }) => (
+  <div className="min-h-screen bg-gray-50 text-gray-800">
+    <Navbar />
+    <main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      {children}
+    </main>
+  </div>
+)
 
 export default Layout
