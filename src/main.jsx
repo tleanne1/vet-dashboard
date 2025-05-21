9990f4b1563afcc80fdea128c10ccbf7a53a8ce1
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2025-05-21 08:55:31 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className="text-center text-2xl font-bold p-10 text-gray-700">Vet Dashboard</div>
+  </React.StrictMode>,
+)
